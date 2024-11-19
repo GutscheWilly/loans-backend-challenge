a42282b1170c9d569b29597362345c286e83bd47
Willy Gutsche <willy.gutsche@unifesp.br>
2024-11-19 16:31:51 -0300

    first commit

---
+++ src/main/java/com/example/loans_backend_challenge/LoansBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.loans_backend_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansBackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loans_backend_challenge/LoansBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loans_backend_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
